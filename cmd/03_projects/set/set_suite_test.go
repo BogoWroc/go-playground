674f07973ad7e381842134672568356686a1f6e7
Bogumil Zebek <bogowrocpl@gmail.com>
2019-12-04 18:50:13 +0100

    Add set module

---
+++ cmd/03_projects/set/set_suite_test.go
@@ -0,0 +1,12 @@
+package set
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestSet(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Set Suite")
+}
