7a9353f6a29d79ab7fdd0ef3fbfb2785ddff8036
Bogumil Zebek <bogowrocpl@gmail.com>
2019-12-08 09:47:10 +0100

    Count doubles kata

---
+++ cmd/02_codewars/kyu6/countDuplicates.go
@@ -0,0 +1,21 @@
+package kyu6
+
+import "strings"
+
+func duplicate_count(s string) int {
+	//your code goes here
+
+	s = strings.ToLower(s)
+	c := s
+
+	var size = len(s)
+	count := 0
+	for _, e := range s {
+		c = strings.Replace(c, string(e), "", -1)
+		if len(c) < size-1 {
+			count++
+		}
+		size = len(c)
+	}
+	return count //Instead of returning '1', you have to return integer 'i' as answer of solution.
+}
---
+++ cmd/02_codewars/kyu6/kyu6_suite_test.go
@@ -0,0 +1,12 @@
+package kyu6
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestKyu8(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Kyu6 Suite")
+}
