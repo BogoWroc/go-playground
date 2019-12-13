0a9424f363353cbbee959ea41f52240b9288de2c
Bogumil Zebek <bogowrocpl@gmail.com>
2019-12-13 18:48:42 +0100

    Project reorganisation

--- cmd/03_projects/calibrate/calibrate.go
+++ cmd/03_projects/calibrate/calibrate.go
@@ -8,8 +8,12 @@ import (
 type kelvin float64
 type sensor func() kelvin
 
-func fakeSensor() kelvin                       { return kelvin(rand.Intn(151) + 150) }
-func calibrate(s sensor, offset kelvin) sensor { return func() kelvin { return s() + offset } }
+func fakeSensor() kelvin {
+	return kelvin(rand.Intn(151) + 150)
+}
+func calibrate(s sensor, offset kelvin) sensor {
+	return func() kelvin { return s() + offset }
+}
 
 func main() {
 	var offset kelvin = 5
